59c91566ca4304307d228395c3fb129b4f26bb3b
atatec <takeata@apost.plala.or.jp>
2020-07-04 15:22:07 +0900

    my first commit

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+pl_pos=1
+com_pos=1
+def banmen():
+    print("・"*(pl_pos-1)+"P"+"・"*(30-pl_pos)+"Goal")
+    print("・"*(com_pos-1)+"C"+"・"*(30-com_pos)+"Goal")
+
+banmen()
+print("スゴロク、スタート!!")
+
+while True:
+    input("Enterを押すとあなたのコマが進みます")
+    pl_pos+=random.randint(1,6)
+    if pl_pos>30:
+        pl_pos=30
+    banmen()
+    if pl_pos==30:
+        print("あなたの勝ちです")
+        break
+    input("Enterを押すとcomのコマが進みます")
+    com_pos+=random.randint(1,6)
+    if com_pos>30:
+        com_pos=30
+    banmen()
+    if com_pos==30:
+        print("comの勝ちです")
+        break
